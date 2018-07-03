a7046bbfe581546e22248df992cfe8453a1d5eac
Arthur Langereis <puurtuur@me.com>
2018-07-03 18:07:49 +0200

    feat: fully implement encode and decode

--- src/sd-utf8.ts
+++ src/sd-utf8.ts
@@ -5,30 +5,30 @@
  * https://github.com/stardazed/sd-utf8
  */
 
-let encoder: TextEncoder | null | undefined;
+let encoder_: TextEncoder | null | undefined;
 function getTextEncoder() {
-	if (encoder === undefined) {
+	if (encoder_ === undefined) {
 		try {
-			encoder = new TextEncoder();
+			encoder_ = new TextEncoder();
 		}
 		catch (e) {
-			encoder = null;
+			encoder_ = null;
 		}
 	}
-	return encoder;
+	return encoder_;
 }
 
-let decoder: TextDecoder | null | undefined;
+let decoder_: TextDecoder | null | undefined;
 function getTextDecoder() {
-	if (encoder === undefined) {
+	if (decoder_ === undefined) {
 		try {
-			decoder = new TextDecoder();
+			decoder_ = new TextDecoder();
 		}
 		catch (e) {
-			decoder = null;
+			decoder_ = null;
 		}
 	}
-	return decoder;
+	return decoder_;
 }
 
 interface StringSourceData {
@@ -36,14 +36,18 @@ interface StringSourceData {
 	utf8ByteLength: number;
 }
 
-function preflight(source: string): StringSourceData {
+/**
+ * Convert the source string to Unicode code points and immediately
+ * determine how long the resulting utf8 byte stream will be.
+ */
+function preflightEncode(source: string): StringSourceData {
 	const length = source.length;
-	const codepoints = new Uint32Array(length);
+	const codepoints = new Uint32Array(length); // worst case length, 1 codepoint per utf-16 code unit
 	let bytesForCodePoint = 0, utf8ByteLength = 0;
 	let hi, lo, codepoint;
-	let ix = -1;
-	while (ix < length - 1) {
-		hi = source.charCodeAt(++ix);
+	let sx = -1, dx = 0;
+	while (sx < length - 1) {
+		hi = source.charCodeAt(++sx);
 		if (hi < 0xD800 || hi > 0xDFFF) {
 			codepoint = hi;
 		}
@@ -52,11 +56,11 @@ function preflight(source: string): StringSourceData {
 			codepoint = 0xFFFD;
 		}
 		else {
-			if (ix === length - 1) {
+			if (sx === length - 1) {
 				codepoint = 0xFFFD;
 			}
 			else {
-				lo = source.charCodeAt(++ix);
+				lo = source.charCodeAt(++sx);
 				if (lo >= 0xDC00 && lo <= 0xDFFF) {
 					codepoint = 0x10000 + ((hi & 0x3FF) << 10) | (lo & 0x3FF);
 				}
@@ -66,25 +70,30 @@ function preflight(source: string): StringSourceData {
 			}
 		}
 
-		codepoints[ix] = codepoint;
+		codepoints[dx++] = codepoint;
 		bytesForCodePoint = (codepoint > 0xFFFF) ? 4 : ((codepoint > 0x7FF) ? 3 : ((codepoint > 0x7F) ? 2 : 1));
 		utf8ByteLength += bytesForCodePoint;
 	}
-	return { codepoints, utf8ByteLength };
+
+	// create a new view instead of slice(), which copies
+	return { codepoints: new Uint32Array(codepoints.buffer, 0, dx), utf8ByteLength };
 }
 
 /**
  * Encode a string as a utf8 byte sequence
  * @param source the string to encode
+ * @param forceUsePolyfill optional param to force using the polyfill implementation (mostly for testing)
  * @returns a buffer view with the encoded sequence
  */
-export function utf8Encode(source: string) {
-	const encoder = getTextEncoder();
-	if (encoder) {
-		return encoder.encode(source);
+export function utf8Encode(source: string, forceUsePolyfill?: boolean) {
+	if (forceUsePolyfill !== true) {
+		const encoder = getTextEncoder();
+		if (encoder) {
+			return encoder.encode(source);
+		}
 	}
 
-	const { codepoints, utf8ByteLength } = preflight(source);
+	const { codepoints, utf8ByteLength } = preflightEncode(source);
 	const data = new Uint8Array(utf8ByteLength);
 	const length = codepoints.length;
 
@@ -113,31 +122,131 @@ export function utf8Encode(source: string) {
 	return data;
 }
 
+const enum ScanMode {
+	Next = 0,
+	OneMore = 1,
+	TwoMore = 2,
+	ThreeMore = 3
+}
+
+// this function is basically convertBytesToString from @stardazed/container
+function codeUnitsToString(codeunits: Uint16Array) {
+	const maxBlockSize = 65536; // max parameter array size for use in Webkit
+	const strings: string[] = [];
+	let unitsLeft = codeunits.length;
+	let offset = 0;
+
+	while (unitsLeft > 0) {
+		const blockSize = Math.min(unitsLeft, maxBlockSize);
+		const str: string = String.fromCharCode.apply(null, codeunits.subarray(offset, offset + blockSize));
+		strings.push(str);
+		offset += blockSize;
+		unitsLeft -= blockSize;
+	}
+
+	return strings.length === 1 ? strings[0] : strings.join("");
+}
+
 /**
  * Decode a string from a utf8 byte sequence
  * @param source the buffer or buffer view with the utf8 data
+ * @param forceUsePolyfill optional param to force using the polyfill implementation (mostly for testing)
  * @returns the decoded string
  */
-export function utf8Decode(source: ArrayBuffer | Uint8Array) {
-	const decoder = getTextDecoder();
-	if (decoder) {
-		return decoder.decode(source);
+export function utf8Decode(source: ArrayBuffer | Uint8Array, forceUsePolyfill?: boolean) {
+	if (forceUsePolyfill !== true) {
+		const decoder = getTextDecoder();
+		if (decoder) {
+			return decoder.decode(source);
+		}
 	}
-/*
-	var result = "";
-	if (!data || (data.length == 0))
-		return result;
 
-	var dix = -1, cc2c = ByteStream._CC2C;
-	while(dix <= data.length - 1) {
-		var sig = data[++dix];
-		if(sig < 0x80)
-			result += cc2c[sig];
-		else if((sig & 0xE0) == 0xC0)
-			result += String.fromCharCode(((sig - 0xC0) << 6) + (data[++dix] & 0x3F));
-		else
-			result += String.fromCharCode(((sig - 0xE0) << 12) + ((data[++dix] & 0x3F) << 6) + (data[++dix] & 0x3F));
+	if (source instanceof ArrayBuffer) {
+		source = new Uint8Array(source);
 	}
-	return result;
-*/
+	const length = source.length;
+	if (source.length === 0) {
+		return "";
+	}
+
+	// worst case count of utf-16 codeunits from utf8 bytes
+	// if all bytes end up as placeholders
+	const codeunits = new Uint16Array(length); 
+
+	let sx = 0, dx = 0;
+	let codepoint = 0;
+	let sequenceStart = 0;
+	let mode = ScanMode.Next;
+
+	while (sx < length) {
+		const byte = source[sx];
+		if (mode === ScanMode.Next) {
+			sequenceStart = sx;
+			if (byte < 0x80) {
+				// 1 byte
+				codepoint = byte & 0x7F;
+			}
+			else if ((byte & 0xE0) === 0xC0) {
+				// 2 bytes
+				if (length - sx > 1) {
+					codepoint = (byte & 0x1F) << 6;
+					mode = ScanMode.OneMore;
+				}
+				else {
+					codepoint = 0xFFFD;
+				}
+			}
+			else if ((byte & 0xF0) === 0xE0) {
+				// 3 bytes
+				if (length - sx > 2) {
+					codepoint = (byte & 0x0F) << 12;
+					mode = ScanMode.TwoMore;
+				}
+				else {
+					codepoint = 0xFFFD;
+				}
+			}
+			else {
+				// 4 bytes
+				if (length - sx > 3) {
+					codepoint = (byte & 0x07) << 18;
+					mode = ScanMode.ThreeMore;
+				}
+				else {
+					codepoint = 0xFFFD;
+				}
+			}
+		}
+		else {
+			// continuation bytes
+			if ((byte & 0xC0) === 0x80) {
+				mode -= 1;
+				codepoint |= ((byte & 0x3F) << (6 * mode));
+			}
+			else {
+				// output placeholder for sequence start byte
+				codepoint = 0xFFFD;
+				mode = ScanMode.Next;
+				// rewind to sequence start byte (will be +1d after this)
+				sx = sequenceStart;
+			}
+		}
+
+		sx += 1;
+		if (mode === ScanMode.Next) {
+			if (codepoint >= 0x10000) {
+				codeunits[dx++] = ((codepoint - 0x10000) >> 10) | 0xD800;
+				codeunits[dx++] = (codepoint & 0x3FF) | 0xDC00;
+			}
+			else {
+				// special case, the zero-width non-breaking space yields nothing
+				if (codepoint !== 0xFEFF) {
+					codeunits[dx++] = codepoint;
+				}
+			}
+		}
+	}
+
+	// create a new view instead of slice(), which copies
+	return codeUnitsToString(new Uint16Array(codeunits.buffer, 0, dx));
 }
